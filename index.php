51a39392146bf039e444533f877e3338d706190f
technerdlove <technerdlove@users.noreply.github.com>
2018-07-05 19:19:26 -0700

    PHP demo survey
    The List page will:
    
    Provide loaded links to the View page for every survey
    Identify the Date Created, Title, Description and Creator's Name
    (Admin's First & Last Name) of each survey
    Data inside the List page must be tabular data (included in HTML tables
    with TH elements)
    Become the index.php file inside of the surveys folder
    
    The View page will:
    
    Implement the Survey classes inside a survey_view.php page for data
    access
    Defend itself from invalid entry by redirecting users back to the List
    page

--- index.php
+++ index.php
@@ -1,1 +1,168 @@
 
+<?php
+/**
+ * demo_list_pager.php along with demo_view_pager.php provides a sample web application
+ *
+ * The difference between demo_list.php and demo_list_pager.php is the reference to the 
+ * Pager class which processes a mysqli SQL statement and spans records across multiple  
+ * pages. 
+ *
+ * The associated view page, demo_view_pager.php is virtually identical to demo_view.php. 
+ * The only difference is the pager version links to the list pager version to create a 
+ * separate application from the original list/view. 
+ * 
+ * @package nmPager
+ * @author technerdlove
+ * @version 1.0 2016/05
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
+ * @see demo_view_pager.php
+ * @see Pager.php 
+ * @todo none
+ */
+
+# '../' works for a sub-folder.  use './' for the root  
+require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials 
+ 
+# SQL statement
+//$sql = "select MuffinName, MuffinID, Price from test_Muffins";
+#ORIGINAL SURVEY SQL
+//$sql = "select Title, Description, SurveyID from sm15_surveys";
+
+#NEW SURVEY SQL
+$sql = "select CONCAT(a.FirstName, ' ', a.LastName) AdminName, s.SurveyID, s.Title, s.Description, 
+date_format(s.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from "
+. PREFIX . "surveys s, " . PREFIX . "Admin a where s.AdminID=a.AdminID order by s.DateAdded desc";
+
+#Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
+$config->titleTag = 'Surveys made with love & PHP in Seattle';
+
+#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
+$config->metaDescription = 'Seattle Central\'s ITC250 Class Surveys are made with pure PHP! ' . $config->metaDescription;
+$config->metaKeywords = 'Surveys,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
+
+/*
+$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.n Central,ITC281,database,mysql,php';
+$config->metaRobots = 'no index, no follow';
+$config->loadhead = ''; #load page specific JS
+$config->banner = ''; #goes inside header
+$config->copyright = ''; #goes inside footer
+$config->sidebar1 = ''; #goes inside left side of page
+$config->sidebar2 = ''; #goes inside right side of page
+$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
+$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
+*/
+
+# END CONFIG AREA ---------------------------------------------------------- 
+
+get_header(); #defaults to theme header or header_inc.php
+?>
+
+<?php
+#START INDEX (LIST )----------------------------------------------
+/*
+<h3 align="center"><?=smartTitle();?></h3>
+
+<p>This page, along with <b>demo_view_pager.php</b>, demonstrate a List/View web application.</p>
+<p>It was built on the mysql shared web application page, <b>demo_shared.php</b></p>
+<p>This page is the entry point of the application, meaning this page gets a link on your web site.  Since the current subject is muffins, we could name the link something clever like <a href="<?php echo VIRTUAL_PATH; ?>demo_list_pager.php">Muffins</a></p>
+<p>Use <b>demo_list_pager.php</b> and <b>demo_view_pager.php</b> as a starting point for building your own List/View web application!</p> 
+*/
+
+/*
+# Read the value of 'action' whether it is passed via $_POST or $_GET with $_REQUEST
+if(isset($_REQUEST['act'])){$myAction = (trim($_REQUEST['act']));}else{$myAction = "";}
+
+switch ($myAction) 
+{//check 'act' for type of process
+	case "add": //2) Form for adding new customer data
+	 	addForm();
+	 	break;
+	case "insert": //3) Insert new customer data
+		insertExecute();
+		break; 
+	default: //1)Show existing customers
+	 	showSurveys();
+}
+
+*/
+//echo '<h3 align="center">' . smartTitle() . '</h3>';
+echo '<h3 align="center">Survey List</h3>';
+
+showSurveys();
+
+function showSurveys()
+{//Select Customer
+
+	//global $config;
+	
+
+	#NEW SURVEY SQL
+	$sql = "select CONCAT(a.FirstName, ' ', a.LastName) AdminName, s.SurveyID, s.Title, s.Description, 
+	date_format(s.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from "
+	. PREFIX . "surveys s, " . PREFIX . "Admin a where s.AdminID=a.AdminID order by s.DateAdded desc";
+	$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+	if (mysqli_num_rows($result) > 0)//at least one record!
+	{//show results
+		echo '<table align="center" border="1" style="border-collapse:collapse" cellpadding="3" cellspacing="3">';
+		echo '<tr>
+				<th>Survey ID</th>
+				<th>Creator Name</th>
+				<th>Title</th>
+				<th>Description</th>
+				<th>Date Created</th>
+			</tr>
+			';
+		while ($row = mysqli_fetch_assoc($result))
+		{//dbOut() function is a 'wrapper' designed to strip slashes, etc. of data leaving db
+			echo '<tr>
+					<td>'	
+				     . (int)$row['SurveyID'] . '</td>
+				    <td>' . dbOut($row['AdminName']) . '</td>
+				    <td>' . dbOut($row['Title']) . '</td>
+				    <td>' . dbOut($row['Description']) . '</td>
+					<td>' . dbOut($row['DateAdded']) . '</td>
+				</tr>
+				';
+		}
+		echo '</table>';
+	}
+	@mysqli_free_result($result); //free resources
+}
+
+#END INDEX CODE------------------------------------------------------
+?>
+
+<?php
+
+#START PAGER CODE-----------------------------------------------------------------------------
+
+#reference images for pager
+$prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
+$next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
+
+# Create instance of new 'pager' class
+$myPager = new Pager(10,'',$prev,$next,''); //number is how many records per page. When you have 10 surveys, you will see the arrows.
+$sql = $myPager->loadSQL($sql);  #load SQL, add offset
+
+# connection comes first in mysqli (improved) function
+$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+if(mysqli_num_rows($result) > 0)
+{#records exist - process
+	if($myPager->showTotal()==1){$itemz = "survey";}else{$itemz = "surveys";}  //deal with plural
+    echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
+	while($row = mysqli_fetch_assoc($result))
+	{# process each row
+         echo '<div align="center"><a href="' . VIRTUAL_PATH . 'surveys/survey_view.php?id=' . (int)$row['SurveyID'] . '">' . dbOut($row['Title']) . '</a>';
+         echo ' </div>';
+	}
+	echo $myPager->showNAV(); # show paging nav, only if enough records	 
+}else{#no records
+    echo "<div align=center>There are currently no surveys</div>";	
+}
+@mysqli_free_result($result);
+
+get_footer(); #defaults to theme footer or footer_inc.php
+
+#END PAGER CODE------------------------------------------------------
+?>
